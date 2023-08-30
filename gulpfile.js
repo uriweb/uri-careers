3c218232b8da53997144e421ae781718f976f0c6
alexandragauss <alexandra_gauss@uri.edu>
2023-08-30 13:58:03 -0400

    Update plugin name, install dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -77,7 +77,7 @@ gulp.task('version', version);
 
 function version(done) {
 
-	gulp.src('./uri-plugin-template.php')
+	gulp.src('./uri-careers.php')
 		.pipe(replace({
 			patterns: [{
 				match: /Version:\s([^\n\r]*)/,
---
+++ uri-careers.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: URI Careers
+ * Plugin URI: http://www.uri.edu
+ * Description: A plugin to display Major and Career data
+ * Version: 0.1.0
+ * Author: URI Web Communications
+ * Author URI: https://today.uri.edu/
+ *
+ * @author: Brandon Fuller <bjcfuller@uri.edu>
+ * @author: Alexandra Gauss <alexandra_gauss@uri.edu>
+ * @author: Sarah Pucino <sarah.pucino@uri.edu>
+ * @package uri-careers
+ */
+
+// Block direct requests
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+/**
+ * Include css and js
+ */
+function uri_careers_enqueues() {
+
+	wp_register_style( 'uri-careers-css', plugins_url( '/css/style.built.css', __FILE__ ) );
+	wp_enqueue_style( 'uri-careers-css' );
+
+	wp_register_script( 'uri-careers-js', plugins_url( '/js/script.built.js', __FILE__ ) );
+	wp_enqueue_script( 'uri-careers-js' );
+
+}
+add_action( 'wp_enqueue_scripts', 'uri_careers_enqueues' );
