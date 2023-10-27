e101de6b668aff930c178e7a81ca7a65e4fbe84a
alexandragauss <alexandra_gauss@uri.edu>
2023-10-27 15:38:01 -0400

    Add cache buster

--- uri-careers.php
+++ uri-careers.php
@@ -21,14 +21,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'URI_CAREERS_PATH', plugin_dir_path( __FILE__ ) );
 
 /**
+ * Returns version from package.json to be used for cache busting
+ *
+ * @return str
+ */
+function uri_careers_cache_buster() {
+	static $cache_buster;
+	if ( empty( $cache_buster ) && function_exists( 'get_plugin_data' ) ) {
+		$values = get_plugin_data( URI_CAREERS_PATH . 'uri-careers.php', false );
+		$cache_buster = $values['Version'];
+	} else {
+		$cache_buster = gmdate( 'Ymd', strtotime( 'now' ) );
+	}
+	return $cache_buster;
+}
+
+/**
  * Include css and js
  */
 function uri_careers_enqueues() {
 
-	wp_register_style( 'uri-careers-css', plugins_url( '/css/style.built.css', __FILE__ ) );
+	wp_register_style( 'uri-careers-css', plugins_url( '/css/style.built.css', __FILE__ ), array(), uri_cl_cache_buster(), 'all' );
 	wp_enqueue_style( 'uri-careers-css' );
 
-	wp_register_script( 'uri-careers-js', plugins_url( '/js/script.built.js', __FILE__ ) );
+	wp_register_script( 'uri-careers-js', plugins_url( '/js/script.built.js', __FILE__ ), array(), uri_cl_cache_buster(), true );
 	wp_enqueue_script( 'uri-careers-js' );
 
 }
