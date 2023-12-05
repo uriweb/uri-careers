c763b39d230c721c1938debd94cf6a7bc119f659
alexandragauss <alexandra_gauss@uri.edu>
2023-12-05 10:51:58 -0500

    Add conditionals to fields

--- inc/uri-careers-helpers.php
+++ inc/uri-careers-helpers.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * URI CAREERS HELPER FUNCTIONS
  *
@@ -6,10 +7,10 @@
  */
 
 function uri_careers_render_jobs( $field ) {
-				$i_array = get_field( $field );
-				$t_array = str_getcsv( $i_array, ';' );
-				$arraylength = count( $t_array );
-				$output3 = null;
+	$i_array = get_field( $field );
+	$t_array = str_getcsv( $i_array, ';' );
+	$arraylength = count( $t_array );
+	$output3 = null;
 
 	for ( $x = 0; $x < $arraylength; $x++ ) {
 		$inner_array = explode( ',', $t_array[ $x ] );
@@ -50,13 +51,43 @@ function uri_careers_pipelist( $name_field ) {
 	$i_array = get_field( $name_field );
 	$t_array = str_getcsv( $i_array, ';' );
 	$output = implode( '&nbsp; | &nbsp;', $t_array );
-	echo $output;
+	return $output;
+}
+
+function uri_careers_render_alumni_data() {
+	 $uri_employers = uri_careers_pipelist( 'employers' );
+	$uri_grad_schools = uri_careers_pipelist( 'grad_schools' );
+	$alumni_data = <<<head
+	<h2 class="bigger-header">Alumni Data</h2>
+	head;
+
+	$alumni_employers = <<<employers
+								<h3>Employers Hiring Our Grads</h3>
+							<div class="pipelist">
+							{$uri_employers}
+							</div>
+							employers;
+
+		$alumni_grad_school = <<<g_school
+							<h3>Graduate Schools Enrolling Our Students</h3>
+							<div class="pipelist">
+							{$uri_grad_schools}
+							</div>
+							g_school;
+		$output = $alumni_data;
+	if ( get_field( 'employers' ) ) {
+			$output .= $alumni_employers;
+	}
+	if ( get_field( 'grad_schools' ) ) {
+		$output .= $alumni_grad_school;
+	}
+	return $output;
 }
 
 function uri_careers_table_template( $entry, $experienced ) {
 	$uri_careers_render_jobs = 'uri_careers_render_jobs';
 	$tabledata = <<<table
-					<h5>Entry Level</h5>
+					<h3>Entry Level</h3>
 						<figure class="wp-block-table">
 						<table style="width: 60%;">
 						<thead>
@@ -68,7 +99,7 @@ function uri_careers_table_template( $entry, $experienced ) {
 								{$uri_careers_render_jobs($entry)}
 					</table>
 					</figure>
-		<h5>Experienced</h5>
+		<h3>Experienced</h3>
 		<figure class="wp-block-table">
 					<table style="width:60%;">
 					<thead>
@@ -86,12 +117,13 @@ function uri_careers_table_template( $entry, $experienced ) {
 }
 
 
-function render_skills() {
+function uri_careers_render_skills() {
 	$major = the_title( '', ' ', false );
 	$uri_careers_skills_list = 'uri_careers_skills_list';
 
 	$skills = <<<content
-	<h4>General competencies:</h4>
+	<div class="skills-columns">
+	<h3>General competencies:</h3>
 			<div class="wp-block-columns">
 		<div class="wp-block-column">	
 	<ul>
@@ -99,22 +131,42 @@ function render_skills() {
 		<li>Communication</li>
 		<li>Teamwork</li>
 		<li>Leadership</li>
-		<li>Technology</li>
 		</ul>
 		</div>
 		<div class="wp-block-column">
 		<ul>
+		<li>Technology</li>
 		<li>Professionalism</li>
 		<li>Career & self development</li>
 		<li>Equity & inclusion</li>
 	</ul>
 	</div>
 	</div>
-	<h4> $major specific competencies:</h4>
+	<h3> $major specific competencies:</h3>
 	<ul>
 	{$uri_careers_skills_list('skills')}
 	</ul>
+	</div>
 	content;
 
 	return $skills;
+}
+
+function uri_careers_render_cards() {
+	$card_1 = '[cl-card title="Center for Career & Experiential Education" body="" img="http://d4.local/wp-content/uploads/2023/10/Screenshot-2023-10-17-at-2.12.50 PM.png" link="https://web.uri.edu/career/" button="Learn More"]';
+	$card_2 = '[cl-card title="Academic Enhancement Center" body="" img="http://d4.local/wp-content/uploads/2023/10/Screenshot-2023-10-17-at-2.12.50 PM.png" link="https://web.uri.edu/aec/" button="Learn More"]';
+
+	$card = <<<cards_content
+	<div class="three-cards">
+	<div class="wp-block-columns">
+								<div class="wp-block-column">
+								{$card_1}
+</div>
+<div class="wp-block-column">
+{$card_2}
+</div>
+</div>
+</div>
+cards_content;
+	return $card;
 }
--- templates/single-major.php
+++ templates/single-major.php
@@ -22,112 +22,83 @@ get_header();
 				?>
 			</div>
 
-			<h2 class="jobs">Top Careers for URI Graduates in <?php the_title(); ?> </h2>
+			<div class="career-data">
+
+				<h2 class="jobs">Top Careers for URI Graduates in <?php the_title(); ?> </h2>
+
+				<?php
+				if ( function_exists( 'uri_cl_shortcode_tabs' ) ) {
+					if ( get_field( 'industry_a_name' ) ) {
+						$industry_a = get_field( 'industry_a_name' );
+
+						if ( get_field( 'industry_a_entry_jobs' ) || get_field( 'industry_a_experienced_jobs ' ) ) {
+							$industry_a_jobs = uri_careers_table_template( 'industry_a_entry_jobs', 'industry_a_experienced_jobs' );
+						}
+						$build_shortcode = '[cl-tab]<h2>' . $industry_a . '</h2>' . $industry_a_jobs . '[/cl-tab]';
+					}
+					if ( get_field( 'industry_b_name' ) ) {
+						$industry_b = get_field( 'industry_b_name' );
+
+						if ( get_field( 'industry_b_entry_jobs' ) || get_field( 'industry_b_experienced_jobs ' ) ) {
+							$industry_b_jobs = uri_careers_table_template( 'industry_b_entry_jobs', 'industry_b_experienced_jobs' );
+						}
+						$build_shortcode .= '[cl-tab]<h2>' . $industry_b . '</h2>' . $industry_b_jobs . '[/cl-tab]';
+					}
+					if ( get_field( 'industry_c_name' ) ) {
+						$industry_c = get_field( 'industry_c_name' );
+
+						if ( get_field( 'industry_c_entry_jobs' ) || get_field( 'industry_c_experienced_jobs ' ) ) {
+							$industry_c_jobs = uri_careers_table_template( 'industry_c_entry_jobs', 'industry_c_experienced_jobs' );
+						}
+						$build_shortcode .= '[cl-tab]<h2>' . $industry_c . '</h2>' . $industry_c_jobs . '[/cl-tab]';
+					}
+					echo do_shortcode( '[cl-tabs]' . $build_shortcode . '[/cl-tabs]' );
+				}
+				?>
+			</div>
+
 
 			<?php
-			if ( function_exists( 'uri_cl_shortcode_tabs' ) ) {
-				if ( get_field( 'industry_a_name' ) ) {
-					$industry_a = get_field( 'industry_a_name' );
-
-					if ( get_field( 'industry_a_entry_jobs' ) || get_field( 'industry_a_experienced_jobs ' ) ) {
-						$industry_a_jobs = uri_careers_table_template( 'industry_a_entry_jobs', 'industry_a_experienced_jobs' );
-					}
-					$build_shortcode = null;
-					$build_shortcode .= '[cl-tab title="' . $industry_a . '"]' . $industry_a_jobs . '[/cl-tab]';
+			if ( get_field( 'employers' ) || ( get_field( 'grad_schools' ) ) ) {
+				if ( function_exists( 'uri_cl_shortcode_breakout' ) ) {
+					$alumni_content = uri_careers_render_alumni_data();
+					echo do_shortcode( '[cl-breakout]' . $alumni_content . '[/cl-breakout]' );
 				}
-				if ( get_field( 'industry_b_name' ) ) {
-					$industry_b = get_field( 'industry_b_name' );
-
-					if ( get_field( 'industry_b_entry_jobs' ) || get_field( 'industry_b_experienced_jobs ' ) ) {
-						$industry_b_jobs = uri_careers_table_template( 'industry_b_entry_jobs', 'industry_b_experienced_jobs' );
-					}
-					$build_shortcode .= '[cl-tab title="' . $industry_b . '"]' . $industry_b_jobs . '[/cl-tab]';
-				}
-				if ( get_field( 'industry_c_name' ) ) {
-					$industry_c = get_field( 'industry_c_name' );
-
-					if ( get_field( 'industry_c_entry_jobs' ) || get_field( 'industry_c_experienced_jobs ' ) ) {
-						$industry_c_jobs = uri_careers_table_template( 'industry_c_entry_jobs', 'industry_c_experienced_jobs' );
-					}
-					$build_shortcode .= '[cl-tab title="' . $industry_c . '"]' . $industry_c_jobs . '[/cl-tab]';
-				}
-				echo do_shortcode( '[cl-tabs]' . $build_shortcode . '[/cl-tabs]' );
 			}
 			?>
 
-			
-<?php if ( get_field( 'employers' ) || ( get_field( 'grad_schools' ) ) ) { ?>
-	<div class="alumni-data">
-	<h3>Alumni Data</h3>
-
-	<?php
-	// check for Employers hiring grads field
-	if ( get_field( 'employers' ) ) {
-		?>
-		<h4>Employers Hiring Our Grads</h4>
-				<div class="pipelist">
-				<?php uri_careers_pipelist( 'employers' ); ?>
-			</div>
-			<?php
-	}
-	// check for graduate schools field
-	if ( get_field( 'grad_schools' ) ) {
-		?>
-				<h4>Graduate Schools Enrolling Our Students</h4>
-				<div class="pipelist">
-				<?php uri_careers_pipelist( 'grad_schools' ); ?>
-			</div>
-			<?php } ?>
-	 </div> <!--end alumni-data -->
-	<?php
-}
-?>
-			
 
 			<?php if ( get_field( 'skills' ) ) { ?>
 				<div class="skills-panel">
-				<?php
-
-				if ( function_exists( 'uri_cl_shortcode_panel' ) ) {
-					$all_skills = render_skills();
-					echo do_shortcode(
-						'[cl-panel title="Skills Employers Desire" img="http://d4.local/wp-content/uploads/2023/10/vecteezy_a-colorful-stack-of-textbooks-on-a-desk-background-with_29288801_805.jpg"]
-					' . $all_skills . '[/cl-panel]'
-						);
-				}
-				?>
+					<div class="skills-list">
+						<h2 class="bigger-header">Desirable Skills</h2>
+						<?php
+						$all_skills = uri_careers_render_skills();
+						echo $all_skills;
+						?>
+					</div>
 				</div>
 				<?php
+			}
+			?>
+
+
+
+
+			<?php
+			if ( function_exists( 'uri_cl_shortcode_breakout' ) ) {
+				if ( get_field( 'featured_story' ) ) {
+					$story = get_field( 'featured_story' );
+
+					echo do_shortcode( '[cl-breakout]<h2 class="bigger-header">Student/Alumni Story</h2>' . $story . '[/cl-breakout]' );
+				}
 			}
 			?>
 
 			<?php
-			if ( function_exists( 'uri_cl_shortcode_breakout' ) ) {
-				$story = get_field( 'featured_story' );
-				echo do_shortcode( '[cl-breakout]<h4>Student/Alumni Story</h4>' . $story . '[/cl-breakout]' );
-				if ( get_field( 'featured_story' ) ) {
-					?>
-					<?php
-				}
-			}
-			?>
-
-			<?php if ( function_exists( 'uri_cl_shortcode_card' ) ) { ?>
-				<div class="three-cards">
-					<div class="wp-block-columns">
-						<div class="wp-block-column">
-							<?php echo do_shortcode( '[cl-card title="Center for Career & Experiential Education" body="" img="http://d4.local/wp-content/uploads/2023/10/Screenshot-2023-10-17-at-2.12.50 PM.png" link="https://web.uri.edu/career/" button="Learn More"]' ); ?>
-						</div>
-						<div class="wp-block-column">
-							<?php echo do_shortcode( '[cl-card title="Academic Enhancement Center" body="" img="http://d4.local/wp-content/uploads/2023/10/Screenshot-2023-10-17-at-2.12.50 PM.png" link="https://web.uri.edu/aec/" button="Learn More"]' ); ?>
-						</div>
-					</div>
-
-
-
-				</div>
-
-				<?php
+			if ( function_exists( 'uri_cl_shortcode_card' ) && function_exists( 'uri_cl_shortcode_breakout' ) ) {
+				$cards = uri_careers_render_cards();
+				echo do_shortcode( '[cl-breakout]' . $cards . '[/cl-breakout]' );
 			}
 			?>
 	</article>
