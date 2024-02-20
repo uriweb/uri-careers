00f9e1ff9017d8400260518efbbe4846fa76efad
alexandragauss <alexandra_gauss@uri.edu>
2024-02-20 15:06:43 -0500

    Remove unused sections of template

--- templates/single-major.php
+++ templates/single-major.php
@@ -43,51 +43,51 @@ get_header();
 			if ( get_field( 'industry_a_name' ) || get_field( 'industry_b_name' ) || get_field( 'industry_c_name' ) ) {
 				?>
 
-			<div class="career-data">
+				<div class="career-data">
 
-				<h2 class="jobs">What Can You Do With This Major?</h2>
-				<p>We gathered data from our past graduates and found that most URI alumni who majored in <?php the_title(); ?> go on to work in these industries among others. </p>
+					<h2 class="jobs">What Can You Do With This Major?</h2>
+					<p>We gathered data from our past graduates and found that most URI alumni who majored in <?php the_title(); ?> go on to work in these industries among others. </p>
 
-				<?php
-				if ( function_exists( 'uri_cl_shortcode_tabs' ) ) {
-					if ( get_field( 'industry_a_name' ) ) {
-						$industry_a = get_field( 'industry_a_name' );
+					<?php
+					if ( function_exists( 'uri_cl_shortcode_tabs' ) ) {
+						if ( get_field( 'industry_a_name' ) ) {
+							$industry_a = get_field( 'industry_a_name' );
 
-						if ( get_field( 'industry_a_entry_jobs' ) ) {
-							$industry_a_jobs .= uri_careers_table_template_entry( 'industry_a_entry_jobs' );
+							if ( get_field( 'industry_a_entry_jobs' ) ) {
+								$industry_a_jobs .= uri_careers_table_template_entry( 'industry_a_entry_jobs' );
+							}
+							if ( get_field( 'industry_a_experienced_jobs' ) ) {
+								$industry_a_jobs .= uri_careers_table_template_experienced( 'industry_a_experienced_jobs' );
+							}
+							$build_shortcode = '[cl-tab]<h2 class="career-name">' . $industry_a . '</h2>' . $industry_a_jobs . '[/cl-tab]';
 						}
-						if ( get_field( 'industry_a_experienced_jobs' ) ) {
-							$industry_a_jobs .= uri_careers_table_template_experienced( 'industry_a_experienced_jobs' );
+						if ( get_field( 'industry_b_name' ) ) {
+							$industry_b = get_field( 'industry_b_name' );
+
+							if ( get_field( 'industry_b_entry_jobs' ) ) {
+								$industry_b_jobs = uri_careers_table_template_entry( 'industry_b_entry_jobs' );
+							}
+							if ( get_field( 'industry_b_experienced_jobs' ) ) {
+								$industry_b_jobs .= uri_careers_table_template_experienced( 'industry_b_experienced_jobs' );
+							}
+
+							$build_shortcode .= '[cl-tab]<h2 class="career-name">' . $industry_b . '</h2>' . $industry_b_jobs . '[/cl-tab]';
 						}
-						$build_shortcode = '[cl-tab]<h2 class="career-name">' . $industry_a . '</h2>' . $industry_a_jobs . '[/cl-tab]';
+						if ( get_field( 'industry_c_name' ) ) {
+							$industry_c = get_field( 'industry_c_name' );
+
+							if ( get_field( 'industry_c_entry_jobs' ) ) {
+								$industry_c_jobs .= uri_careers_table_template_entry( 'industry_c_entry_jobs' );
+							}
+							if ( get_field( 'industry_c_experienced_jobs' ) ) {
+								$industry_c_jobs .= uri_careers_table_template_experienced( 'industry_c_experienced_jobs' );
+							}
+							$build_shortcode .= '[cl-tab]<h2 class="career-name">' . $industry_c . '</h2>' . $industry_c_jobs . '[/cl-tab]';
+						}
+						echo do_shortcode( '[cl-tabs]' . $build_shortcode . '[/cl-tabs]' );
 					}
-					if ( get_field( 'industry_b_name' ) ) {
-						$industry_b = get_field( 'industry_b_name' );
-
-						if ( get_field( 'industry_b_entry_jobs' ) ) {
-							$industry_b_jobs = uri_careers_table_template_entry( 'industry_b_entry_jobs' );
-						}
-						if ( get_field( 'industry_b_experienced_jobs' ) ) {
-							$industry_b_jobs .= uri_careers_table_template_experienced( 'industry_b_experienced_jobs' );
-						}
-
-						$build_shortcode .= '[cl-tab]<h2 class="career-name">' . $industry_b . '</h2>' . $industry_b_jobs . '[/cl-tab]';
-					}
-					if ( get_field( 'industry_c_name' ) ) {
-						$industry_c = get_field( 'industry_c_name' );
-
-						if ( get_field( 'industry_c_entry_jobs' ) ) {
-							$industry_c_jobs .= uri_careers_table_template_entry( 'industry_c_entry_jobs' );
-						}
-						if ( get_field( 'industry_c_experienced_jobs' ) ) {
-							$industry_c_jobs .= uri_careers_table_template_experienced( 'industry_c_experienced_jobs' );
-						}
-						$build_shortcode .= '[cl-tab]<h2 class="career-name">' . $industry_c . '</h2>' . $industry_c_jobs . '[/cl-tab]';
-					}
-					echo do_shortcode( '[cl-tabs]' . $build_shortcode . '[/cl-tabs]' );
-				}
-				?>
-			</div>
+					?>
+				</div>
 
 				<?php
 				if ( get_field( 'employers' ) || ( get_field( 'grad_schools' ) ) ) {
@@ -99,25 +99,15 @@ get_header();
 			}
 			?>
 
-				<div class="skills-panel">
-					<div class="skills-list">
-						<h2 class="bigger-header" id="skills">What Do Employers Look for in a Candidate?</h2>
-						<?php
-						$all_skills = uri_careers_render_skills();
-						echo $all_skills;
-						?>
-					</div>
+			<div class="skills-panel">
+				<div class="skills-list">
+					<h2 class="bigger-header" id="skills">What Do Employers Look for in a Candidate?</h2>
+					<?php
+					$all_skills = uri_careers_render_skills();
+					echo $all_skills;
+					?>
 				</div>
-
-			<?php
-			if ( function_exists( 'uri_cl_shortcode_breakout' ) ) {
-				if ( get_field( 'featured_story' ) ) {
-					$story = get_field( 'featured_story' );
-
-					echo do_shortcode( '[cl-breakout]<h2 class="bigger-header">Student/Alumni Story</h2>' . $story . '[/cl-breakout]' );
-				}
-			}
-			?>
+			</div>
 
 			<div class="banner-area">
 				<div class="banner-background use-banner-width">
