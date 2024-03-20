b93b9ca5f24a7fbd05d9122a7270024b9a68a0ef
alexandragauss <alexandra_gauss@uri.edu>
2024-03-20 13:17:36 -0400

    Fix toggle function

--- templates/toggle.php
+++ templates/toggle.php
@@ -3,10 +3,7 @@
 /**
  * Create the toggle between the Advising and Careers page
  */
-function uri_careers_toggle( $link_careers, $link_advising, $button_style, $style_class ) {
-	if ( function_exists( 'uri_cl_shortcode_button' ) ) {
-		echo '<div class="center">';
-		echo do_shortcode( '[cl-button class="' . $style_class[0] . '" link="' . $link_advising . '" text="Majors" style=' . $button_style[0] . '][cl-button class="' . $style_class[1] . '" link="' . $link_careers . '" text="Careers" style="' . $button_style[1] . '"]' );
-		echo '</div>';
-	}
+function uri_careers_toggle( $button_careers, $button_majors, $button_style, $style_class ) {
+	return '<div class="center">' . do_shortcode( '[cl-button class="' . $style_class[0] . '" link="' . $button_majors . '" text="Majors" style=' . $button_style[0] . '][cl-button class="' . $style_class[1] . '" link="' . $button_careers . '" text="Careers" style="' . $button_style[1] . '"]' ) .
+	'</div>';
 }
