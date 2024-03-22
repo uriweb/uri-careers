b3266c619a3890ec9835afd3f9c7c92960aab737
alexandragauss <alexandra_gauss@uri.edu>
2024-03-22 15:15:48 -0400

    Change button text on toggle

--- templates/toggle.php
+++ templates/toggle.php
@@ -4,6 +4,6 @@
  * Create the toggle between the Advising and Careers page
  */
 function uri_careers_toggle( $button_careers, $button_majors, $button_style, $style_class ) {
-	return '<div class="center">' . do_shortcode( '[cl-button class="' . $style_class[0] . '" link="' . $button_majors . '" text="Majors" style=' . $button_style[0] . '][cl-button class="' . $style_class[1] . '" link="' . $button_careers . '" text="Careers" style="' . $button_style[1] . '"]' ) .
+	return '<div class="center">' . do_shortcode( '[cl-button class="' . $style_class[0] . '" link="' . $button_majors . '" text="Major" style=' . $button_style[0] . '][cl-button class="' . $style_class[1] . '" link="' . $button_careers . '" text="Career" style="' . $button_style[1] . '"]' ) .
 	'</div>';
 }
