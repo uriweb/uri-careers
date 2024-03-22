2df207cc3e22e045f9cf682aa7939d5996efd622
alexandragauss <alexandra_gauss@uri.edu>
2024-03-22 15:59:32 -0400

    Change toggle and shortcode attributes/variables to singular

--- templates/toggle.php
+++ templates/toggle.php
@@ -3,7 +3,7 @@
 /**
  * Create the toggle between the Advising and Careers page
  */
-function uri_careers_toggle( $button_careers, $button_majors, $button_style, $style_class ) {
-	return '<div class="center">' . do_shortcode( '[cl-button class="' . $style_class[0] . '" link="' . $button_majors . '" text="Major" style=' . $button_style[0] . '][cl-button class="' . $style_class[1] . '" link="' . $button_careers . '" text="Career" style="' . $button_style[1] . '"]' ) .
+function uri_careers_toggle( $button_career, $button_major, $button_style, $style_class ) {
+	return '<div class="center">' . do_shortcode( '[cl-button class="' . $style_class[0] . '" link="' . $button_major . '" text="Major" style=' . $button_style[0] . '][cl-button class="' . $style_class[1] . '" link="' . $button_career . '" text="Career" style="' . $button_style[1] . '"]' ) .
 	'</div>';
 }
